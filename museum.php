4202c68a4017c889e270c8998318d05f6a247e05
Rafael Funchal <rafaelfunchal@gmail.com>
2017-10-14 14:39:35 -0300

    Add Exhibition Post Type

--- museum.php
+++ museum.php
@@ -14,7 +14,7 @@
 
 /**
  * 
- * Create Collection and Artists Post Types
+ * Create Collection, Artists, and Exhibitions Post Types
  * 
  */
 
@@ -24,9 +24,9 @@ function museum_post_types() {
 	 * Collection
 	 */
 	$collection_labels = array(
-		'name'               => __( 'Collections', 'museum' ),
+		'name'               => __( 'Collection', 'museum' ),
 		'singular_name'      => __( 'Collection', 'museum' ),
-		'add_new'            => _x( 'Add New Item', 'museum' ),
+		'add_new'            => _x( 'Add New', 'museum' ),
 		'add_new_item'       => __( 'Add New Collection Item', 'museum' ),
 		'edit_item'          => __( 'Edit Collection Item', 'museum' ),
 		'new_item'           => __( 'New Collection Item', 'museum' ),
@@ -48,7 +48,7 @@ function museum_post_types() {
 		'show_in_menu'        => true,
 		'show_in_admin_bar'   => true,
 		'menu_position'       => null,
-		'menu_icon'           => 'dashicons-index-card',
+		'menu_icon'           => 'dashicons-art',
 		'show_in_nav_menus'   => true,
 		'publicly_queryable'  => true,
 		'exclude_from_search' => false,
@@ -77,7 +77,7 @@ function museum_post_types() {
 	$artist_labels = array(
 		'name'               => __( 'Artists', 'museum' ),
 		'singular_name'      => __( 'Artist', 'museum' ),
-		'add_new'            => _x( 'Add New Artist', 'museum' ),
+		'add_new'            => _x( 'Add New', 'museum' ),
 		'add_new_item'       => __( 'Add New Artist', 'museum' ),
 		'edit_item'          => __( 'Edit Artist', 'museum' ),
 		'new_item'           => __( 'New Artist', 'museum' ),
@@ -121,5 +121,56 @@ function museum_post_types() {
 	);
 
 	register_post_type( 'artist', $artist_args );
+
+	/**
+	 * Exhibitions
+	 */
+	$exhibition_labels = array(
+		'name'               => __( 'Exhibitions', 'museum' ),
+		'singular_name'      => __( 'Exhibition', 'museum' ),
+		'add_new'            => _x( 'Add New', 'museum' ),
+		'add_new_item'       => __( 'Add New Exhibition', 'museum' ),
+		'edit_item'          => __( 'Edit Exhibition ', 'museum' ),
+		'new_item'           => __( 'New Exhibition', 'museum' ),
+		'view_item'          => __( 'View Exhibition', 'museum' ),
+		'search_items'       => __( 'Search Exhibitions', 'museum' ),
+		'not_found'          => __( 'No Exhibitions found', 'museum' ),
+		'not_found_in_trash' => __( 'No Exhibition found in Trash', 'museum' ),
+		'parent_item_colon'  => __( 'Parent Exhibition:', 'museum' ),
+		'menu_name'          => __( 'Exhibitions', 'museum' ),
+	);
+
+	$exhibition_args = array(
+		'labels'              => $exhibition_labels,
+		'hierarchical'        => false,
+		'description'         => 'description',
+		'taxonomies'          => array(),
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => null,
+		'menu_icon'           => 'dashicons-index-card',
+		'show_in_nav_menus'   => true,
+		'publicly_queryable'  => true,
+		'exclude_from_search' => false,
+		'has_archive'         => true,
+		'query_var'           => true,
+		'can_export'          => true,
+		'rewrite'             => true,
+		'capability_type'     => 'post',
+		'supports'            => array(
+			'title',
+			'editor',
+			'author',
+			'thumbnail',
+			'excerpt',
+			'trackbacks',
+			'comments',
+			'revisions'
+		),
+	);
+
+	register_post_type( 'exhibition', $exhibition_args );
 }
 add_action( 'init', 'museum_post_types' );
